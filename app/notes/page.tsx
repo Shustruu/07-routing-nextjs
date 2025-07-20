8485b2c7546707ea8a864465085dfb3c8ed168d6
Leon <mazleon777@gmail.com>
2025-07-20 12:39:29 +0300

    all

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,16 +2,7 @@ import NotesClient from './Notes.client';
 import { getNotes } from '@/lib/api';
 
 export default async function NotesPage() {
-  const data = await getNotes();
+  const data = await getNotes('', 1); 
 
   return <NotesClient initialData={data} />;
 }
-
-
-
-
-
-
-
-
-
